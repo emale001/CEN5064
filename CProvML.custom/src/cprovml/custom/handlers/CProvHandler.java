77be99275ab1bf856b4498a622d50c02d3168f24
Dionny Santiago <dsant005@fiu.edu>
2015-03-12 23:08:38 -0400

    Added CProvML menu implementation.

---
+++ CProvML.custom/src/cprovml/custom/handlers/CProvHandler.java
@@ -0,0 +1,46 @@
+package cprovml.custom.handlers;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipse.ui.part.FileEditorInput;
+import org.eclipse.jface.dialogs.MessageDialog;
+
+/**
+ * Our sample handler extends AbstractHandler, an IHandler base class.
+ * @see org.eclipse.core.commands.IHandler
+ * @see org.eclipse.core.commands.AbstractHandler
+ */
+public class CProvHandler extends AbstractHandler {
+	/**
+	 * The constructor.
+	 */
+	public CProvHandler() {
+	}
+
+	/**
+	 * the command has been executed, so extract extract the needed information
+	 * from the application context.
+	 */
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
+		IWorkbenchPage page = window.getActivePage();
+		IEditorPart editor = page.getActiveEditor();
+		IEditorInput input = editor.getEditorInput();
+		IPath path = ((FileEditorInput)input).getPath();
+		String cprovmlPath = path.toString().replace(".cprovml_diagram", ".cprovml");
+		
+		MessageDialog.openInformation(
+				window.getShell(),
+				"CProvML Custom Editor",
+				"CProvML Path to be transformed:" + cprovmlPath);
+		
+		return null;
+	}
+}
