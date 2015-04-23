79a517d57b81abed6d3f85ab099ed9517ce7213c
Dionny Santiago <dionny_santiago@ultimatesoftware.com>
2015-04-23 13:08:42 -0400

    X to Text menu.

---
+++ CProvML.custom/src/cprovml/custom/handlers/CProvHandlerCalls.java
@@ -0,0 +1,69 @@
+package cprovml.custom.handlers;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.swing.JFileChooser;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.emf.common.util.Diagnostic;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.util.Diagnostician;
+import org.eclipse.emf.ecore.xmi.XMIResource;
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceImpl;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipse.ui.part.FileEditorInput;
+import org.eclipse.jface.dialogs.MessageDialog;
+
+import cprovml.custom.transformations.APIFactory;
+import cprovml.custom.transformations.XMLParser;
+
+/**
+ * Our sample handler extends AbstractHandler, an IHandler base class.
+ * @see org.eclipse.core.commands.IHandler
+ * @see org.eclipse.core.commands.AbstractHandler
+ */
+public class CProvHandlerCalls extends AbstractHandler {
+	/**
+	 * The constructor.
+	 */
+	public CProvHandlerCalls() {
+	}
+
+	/**
+	 * the command has been executed, so extract extract the needed information
+	 * from the application context.
+	 */
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
+		JFileChooser fc = new JFileChooser();
+		int returnVal = fc.showOpenDialog(null);
+
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
+            File file = fc.getSelectedFile();
+            
+            String directory = file.getParent();
+            
+            // Transform CProvML to API calls.
+    		String callsFileName = file.getName().split("\\.(?=[^\\.]+$)")[0] + ".txt";
+    		String callsOutputName = new File(directory, callsFileName).getAbsolutePath();
+    	
+    		APIFactory.toTXT(file.getAbsolutePath(), callsOutputName);
+    		
+    		MessageDialog.openInformation(
+    				window.getShell(),
+    				"CProvML Custom Editor",
+    				"Transformation was successful.");
+        }
+		
+		return null;
+	}
+}
--- CProvML.custom/src/cprovml/custom/transformations/APIFactory.java
+++ CProvML.custom/src/cprovml/custom/transformations/APIFactory.java
@@ -8,7 +8,6 @@ import java.io.IOException;
 import java.util.HashMap;
 
 
-
 import javax.xml.parsers.DocumentBuilder;  
 import javax.xml.parsers.DocumentBuilderFactory;  
 import javax.xml.parsers.ParserConfigurationException;  
